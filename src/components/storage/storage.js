af3bbcd44934491be8e9f89b1055baa445caec55
Yuji T <accmanager1990@gmail.com>
2022-02-17 05:02:09 +0900

    Format

--- src/components/storage/storage.js
+++ src/components/storage/storage.js
@@ -1,55 +1,58 @@
 const Storage = (type = 'localStorage') => {
-    let _storage;
-    // check if storage is available for use
-    (()=>{
-        try {
-            _storage = window[type];
-            let x = '__storage_test__';
-            _storage.setItem(x, x);
-            _storage.removeItem(x);
-            console.log('storage test cleared')
-        } catch(e) {
-            _storage = null;
-            return e instanceof DOMException && (
-                // everything except Firefox
-                e.code === 22 ||
-                // Firefox
-                e.code === 1014 ||
-                // test name field too, because code might not be present
-                // everything except Firefox
-                e.name === 'QuotaExceededError' ||
-                // Firefox
-                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-                // acknowledge QuotaExceededError only if there's something already stored
-                (_storage && _storage.length !== 0);
-        }
-    })();
-
-    function add(key, value){
-        if(_storage){
-            _storage.setItem(key, value);
-        }
+  let mStorage;
+  // check if storage is available for use
+  (() => {
+    try {
+      mStorage = window[type];
+      let x = '__storage_test__';
+      mStorage.setItem(x, x);
+      mStorage.removeItem(x);
+    } catch (e) {
+      mStorage = null;
+      return (
+        e instanceof DOMException &&
+        // everything except Firefox
+        (e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        mStorage &&
+        mStorage.length !== 0
+      );
     }
+  })();
 
-    function remove(key, value){
-        if(_storage){
-            _storage.removeItem(key, value);
-        }
+  function add(key, value) {
+    if (mStorage) {
+      mStorage.setItem(key, value);
     }
+  }
 
-    function get(key){
-        if(_storage){
-            return _storage.getItem(key)
-        }
+  function remove(key, value) {
+    if (mStorage) {
+      mStorage.removeItem(key, value);
     }
+  }
 
-    function clear(){
-        if(_storage){
-            _storage.clear()
-        }
+  function get(key) {
+    if (mStorage) {
+      return mStorage.getItem(key);
     }
+    return null;
+  }
 
-    return {add, remove, clear, get};
+  function clear() {
+    if (mStorage) {
+      mStorage.clear();
+    }
+  }
+
+  return { add, remove, clear, get };
 };
 
-export {Storage};
+export default Storage;
