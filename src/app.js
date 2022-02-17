ba5944c533bb06ff20f19b6b66a6d28d069cfdcf
Yuji T <accmanager1990@gmail.com>
2022-02-18 02:39:33 +0900

    Add firebase

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
+import './firebase/firebase';
+import { useUser, auth } from './firebase/auth';
+import './firebase/firestore';
 import Header from './components/header/header';
 import Sidebar from './components/sidebar/sidebar';
 import React, { useState } from 'react';
 import Main from './components/main/main';
 import Data from './data';
-import './firebase/firebase';
-import { useUser, auth } from './firebase/auth';
 import './app.css';
 
 const App = () => {
   const userHook = useUser();
-  const data = Data();
+  const data = Data(userHook.user);
   const [selectedGroup, setSelectedGroup] = useState('');
+  // passing auth to header instead of importing auth in header.js because
+  // auth is not initialized at the time of rendering header, i think. well, it threw an error at least
   return (
     <div className="app">
       <Header user={userHook.user} auth={auth} />
